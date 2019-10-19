16dee16e2f805710dc6c0eab906ce3169176842c
Ekrem <ekremugur17@gmail.com>
2019-10-19 22:57:20 +0300

    still trying

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ from passlib.hash import pbkdf2_sha256
 from functools import wraps
 from flask_socketio import SocketIO, send
 
-con = connect(dbname='atarem', user='postgres',
-            host='localhost', password='sudouser')
+con = connect(dbname='de9gpi5nc7pnj5', user='fvpxkozyyyirvo', port='5432',
+            host='ec2-54-217-234-157.eu-west-1.compute.amazonaws.com', password='2c9deabd2e3ceadf157c8cf47204c3aac97fff8d3179dc58d06814489b24fd5a')
 app = Flask(__name__, static_url_path='/static')
 socketio = SocketIO(app)
 
