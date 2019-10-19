b28812a63335f76d73a68ce4ed60753344ddcb5b
Ekrem <ekremugur17@gmail.com>
2019-10-19 21:08:27 +0300

    app construction

--- dbinit.py
+++ dbinit.py
@@ -1,12 +1,37 @@
 import os
 import sys
+import requests
+import json
 
 import psycopg2 as dbapi2
 
-
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """
+    CREATE TABLE IF NOT EXISTS USERS (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    email VARCHAR(35) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    register_date DATE NOT NULL default CURRENT_DATE
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS MOVIES (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR NOT NULL,
+    year INTEGER NOT NULL,
+    directors VARCHAR[] NOT NULL,
+    writers VARCHAR[] NOT NULL,
+    urlPoster VARCHAR NOT NULL,
+    genres VARCHAR[] NOT NULL,
+    plot TEXT NOT NULL,
+    simpleplot TEXT NOT NULL,
+    idIMDB VARCHAR NOT NULL,
+    runtime VARCHAR NOT NULL,
+    rating DOUBLE PRECISION NOT NULL
+    )
+    """,
 ]
 
 
@@ -15,6 +40,21 @@ def initialize(url):
         cursor = connection.cursor()
         for statement in INIT_STATEMENTS:
             cursor.execute(statement)
+        cur = connection.cursor(cursor_factory=dbapi2.extras.DictCursor)
+        resp = requests.get(
+            'https://www.myapifilms.com/imdb/top?start=1&end=250&token=93dd88e2-17fb-40e8-89a3-1707b3c8ac82&format=json&data=1')
+        real = json.loads(resp.text)
+        for movie in real['data']['movies']:
+            directors = []
+            writers = []
+            for director in movie['directors']:
+                directors.append(director['name'])
+            for writer in movie['writers']:
+                writers.append(writer['name'])
+            cur = connection.cursor()
+            cur.execute("INSERT INTO movies (title, year, directors, writers, urlPoster, genres, plot, simpleplot, idIMDB, rating, runtime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                        (movie['title'], movie['year'], directors, writers, movie['urlPoster'], movie['genres'], movie['plot'], movie['simplePlot'], movie['idIMDB'], movie['rating'], movie['runtime']))
+        connection.commit()
         cursor.close()
 
 
--- server.py
+++ server.py
@@ -1,13 +1,143 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, flash, session, logging, request
+from wtforms import Form, StringField, BooleanField, TextAreaField, PasswordField, validators, BooleanField
+from psycopg2 import connect, extras
+from passlib.hash import pbkdf2_sha256
+from functools import wraps
+from flask_socketio import SocketIO, send
+
+con = connect(dbname='atarem', user='postgres',
+              host='localhost', password='sudouser')
+app = Flask(__name__, static_url_path='/static')
+socketio = SocketIO(app)
 
 
-app = Flask(__name__)
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash(
+                'You must login to your account in order to continue browsing', 'warning')
+            return redirect(url_for('login'))
+    return wrap
 
 
-@app.route("/")
-def home_page():
-    return "YA ISTIKLAL YA OLUM!"
+def is_logged_out(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' not in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('dashboard'))
+    return wrap
 
 
-if __name__ == "__main__":
-    app.run()
+@socketio.on('message')
+def handleMessage(msg):
+    send(msg, broadcast=True)
+
+
+@app.route('/')
+@is_logged_out
+def index():
+    return render_template('landing.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+@is_logged_out
+def login():
+    if(request.method == 'POST'):
+        username = request.form['username']
+        password = request.form['password']
+        cur = con.cursor(cursor_factory=extras.DictCursor)
+        cur.execute(
+            "SELECT * FROM users WHERE username='%s'" % (username))
+        data = cur.fetchone()
+        if(data):
+            hash = data['password']
+            if pbkdf2_sha256.verify(password, hash):
+                session['logged_in'] = True
+                session['username'] = username
+                flash('Logged in successfully!', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Invalid Credentials', 'danger')
+        else:
+            flash('Invalid Credentials', 'danger')
+    return render_template('login.html')
+
+
+class RegistrationForm(Form):
+    name = StringField('Name', [validators.Length(min=4, max=16)])
+    username = StringField('Username', [validators.Length(min=4, max=16)])
+    email = StringField('Email Address', [validators.Length(min=6, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo(
+            'confirm', message='Confirmation password do not match'),
+        validators.Length(min=6, max=16)
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+@is_logged_out
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = pbkdf2_sha256.hash(form.password.data)
+        cur = con.cursor()
+        cur.execute("INSERT INTO users (name, username, email, password) VALUES (%s, %s, %s, %s)",
+                    (name, username, email, password))
+        con.commit()
+        cur.close()
+        flash('Registration successful!', 'success')
+        return redirect(url_for('register'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('Logged out successfully', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/discussion')
+@is_logged_in
+def discussion():
+    return render_template('discussion.html')
+
+
+@app.route('/movies')
+def movies():
+    cur = con.cursor(cursor_factory=extras.DictCursor)
+    cur.execute("SELECT * FROM movies")
+    movies = cur.fetchall()
+    cur.close()
+    return render_template('movies.html', movies=movies)
+
+
+@app.route('/movie/<string:id>/')
+def movie(id):
+    cur = con.cursor(cursor_factory=extras.DictCursor)
+    cur.execute(f"SELECT * FROM movies WHERE idimdb='{id}'")
+    movie = cur.fetchone()
+    cur.close()
+    return render_template('movie.html', movie=movie)
+
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = '65vet6'
+    socketio.run(app, debug=True)
